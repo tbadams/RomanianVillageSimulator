066a95dab7e08dfb19bc1b2675fde43d0986a65a
Trevor Adams <trevor.b.adams@gmail.com>
2017-10-09 21:50:32 -0700

    Add ncurses libraries and header files.

---
+++ lib/ncurses/c++/newdemo.cc
@@ -0,0 +1,14 @@
+#include <cursesapp.h>
+#include <cursesm.h>
+#include <cursesf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    NCursesPanel *mystd = new NCursesPanel();
+    cout << "Hello world";
+    return 0;
+}
