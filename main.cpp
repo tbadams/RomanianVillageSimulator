ba016fec2a5275cccba6257d9a595319362386cc
Trevor Adams <trevor.b.adams@gmail.com>
2017-10-10 10:43:47 -0700

    Working curses menu!

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <time.h>
 
 #include <cursesapp.h>
+#include <cursesm.h>
+#include <cursesf.h>
 
 #include "WorldMap.h"
 
@@ -37,111 +39,111 @@ Village* VillageFactory::build() {
     return new Village(rand() % (theMap->getWidth()-1), rand() % (theMap->getHeight()-1));
 }
 
-//class UserData
-//{
-//private:
-//  int u;
-//public:
-//  UserData(int x) : u(x) {}
-//  int sleeptime() const { return u; }
-//};
-//
-//class PassiveItem : public NCursesMenuItem
-//{
-//public:
-//  PassiveItem(const char* text) : NCursesMenuItem(text) {
-//    options_off(O_SELECTABLE);
-//  }
-//};
-//
-//
-//class MyMenu : public NCursesMenu
-//{
-//private:
-//  NCursesPanel* P;
-//  NCursesMenuItem** I;
-//  UserData *u;
-//  #define n_items 2
-//
-//public:
-//  MyMenu ()
-//    : NCursesMenu (n_items+2, 8, (lines()-10)/2, (cols()-10)/2),
-//      P(0), I(0),
-//      u(0)
-//  {
-//    u = new UserData(1);
-//    I = new NCursesMenuItem*[1+n_items];
-//    I[0] = new PassiveItem("One");
-//    I[1] = new PassiveItem("Two");
-////    I[2] = new MyAction<UserData> ("Silly", u);
-////    I[3] = new FormAction("Form");
-////    I[4] = new PadAction("Pad");
-////    I[5] = new ScanAction("Scan");
-////    I[6] = new QuitItem();
-//    I[2] = new NCursesMenuItem(); // Terminating empty item
-//
-//    InitMenu(I, TRUE, TRUE);
-//
-//    P = new NCursesPanel(1, n_items, LINES-1, 1);
-//    boldframe("Demo", "Silly");
-//    P->show();
-//  }
-//
-//  MyMenu& operator=(const MyMenu& rhs)
-//  {
-//    if (this != &rhs) {
-//      *this = rhs;
-//    }
-//    return *this;
-//  }
-//
-//  MyMenu(const MyMenu& rhs)
-//    : NCursesMenu(rhs), P(0), I(0), u(0)
-//  {
-//  }
-//
-//  ~MyMenu()
-//  {
-//    P->hide();
-//    delete P;
-//    delete u;
-//  }
-//
-//  virtual void On_Menu_Init()
-//  {
-//    NCursesWindow W(::stdscr);
-//    P->move(0, 0);
-//    P->clrtoeol();
-//    for(int i=1; i<=count(); i++)
-//      P->addch('0' + i);
-//    P->bkgd(W.getbkgd());
-//    refresh();
-//  }
-//
-//  virtual void On_Menu_Termination()
-//  {
-//    P->move(0, 0);
-//    P->clrtoeol();
-//    refresh();
-//  }
-//
-//  virtual void On_Item_Init(NCursesMenuItem& item)
-//  {
-//    P->move(0, item.index());
-//    P->attron(A_REVERSE);
-//    P->printw("%1d", 1+item.index());
-//    P->attroff(A_REVERSE);
-//    refresh();
-//  }
-//
-//  virtual void On_Item_Termination(NCursesMenuItem& item)
-//  {
-//    P->move(0, item.index());
-//    P->attroff(A_REVERSE);
-//    P->printw("%1d", 1+item.index());
-//    refresh();
-//  }
-//};
+class UserData
+{
+private:
+  int u;
+public:
+  UserData(int x) : u(x) {}
+  int sleeptime() const { return u; }
+};
+
+class PassiveItem : public NCursesMenuItem
+{
+public:
+  PassiveItem(const char* text) : NCursesMenuItem(text) {
+    options_off(O_SELECTABLE);
+  }
+};
+
+
+class MyMenu : public NCursesMenu
+{
+private:
+  NCursesPanel* P;
+  NCursesMenuItem** I;
+  UserData *u;
+  #define n_items 2
+
+public:
+  MyMenu ()
+    : NCursesMenu (n_items+2, 8, (lines()-10)/2, (cols()-10)/2),
+      P(0), I(0),
+      u(0)
+  {
+    u = new UserData(1);
+    I = new NCursesMenuItem*[1+n_items];
+    I[0] = new PassiveItem("One");
+    I[1] = new PassiveItem("Two");
+//    I[2] = new MyAction<UserData> ("Silly", u);
+//    I[3] = new FormAction("Form");
+//    I[4] = new PadAction("Pad");
+//    I[5] = new ScanAction("Scan");
+//    I[6] = new QuitItem();
+    I[2] = new NCursesMenuItem(); // Terminating empty item
+
+    InitMenu(I, TRUE, TRUE);
+
+    P = new NCursesPanel(1, n_items, LINES-1, 1);
+    boldframe("Demo", "Silly");
+    P->show();
+  }
+
+  MyMenu& operator=(const MyMenu& rhs)
+  {
+    if (this != &rhs) {
+      *this = rhs;
+    }
+    return *this;
+  }
+
+  MyMenu(const MyMenu& rhs)
+    : NCursesMenu(rhs), P(0), I(0), u(0)
+  {
+  }
+
+  ~MyMenu()
+  {
+    P->hide();
+    delete P;
+    delete u;
+  }
+
+  virtual void On_Menu_Init()
+  {
+    NCursesWindow W(::stdscr);
+    P->move(0, 0);
+    P->clrtoeol();
+    for(int i=1; i<=count(); i++)
+      P->addch('0' + i);
+    P->bkgd(W.getbkgd());
+    refresh();
+  }
+
+  virtual void On_Menu_Termination()
+  {
+    P->move(0, 0);
+    P->clrtoeol();
+    refresh();
+  }
+
+  virtual void On_Item_Init(NCursesMenuItem& item)
+  {
+    P->move(0, item.index());
+    P->attron(A_REVERSE);
+    P->printw("%1d", 1+item.index());
+    P->attroff(A_REVERSE);
+    refresh();
+  }
+
+  virtual void On_Item_Termination(NCursesMenuItem& item)
+  {
+    P->move(0, item.index());
+    P->attroff(A_REVERSE);
+    P->printw("%1d", 1+item.index());
+    refresh();
+  }
+};
 
 class TestApplication : public NCursesApplication
 {
@@ -173,7 +175,7 @@ public:
 
 void TestApplication::title()
 {
-  const char * const titleText = "Simple C++ Binding Demo";
+  const char * const titleText = "Romanian Village Simulator";
   const int len = ::strlen(titleText);
 
   titleWindow->bkgd(screen_titles());
@@ -184,8 +186,8 @@ void TestApplication::title()
 
 int TestApplication::run()
 {
-//  MyMenu M;
-//  M();
+  MyMenu M;
+  M();
   return 0;
 }
 
